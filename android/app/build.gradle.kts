2ba03ff8d312d5033146136bb1d2657078a7bcf6
Hanny Yosephine <hannyyosephine37@gmail.com>
2025-06-07 10:01:27 +0700

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,47 +1,3 @@
-//plugins {
-//    id("com.android.application")
-//    id("kotlin-android")
-//    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-//    id("dev.flutter.flutter-gradle-plugin")
-//}
-//
-//android {
-//    namespace = "com.example.delivery_app"
-//    compileSdk = flutter.compileSdkVersion
-//    ndkVersion = flutter.ndkVersion
-//
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_11
-//        targetCompatibility = JavaVersion.VERSION_11
-//    }
-//
-//    kotlinOptions {
-//        jvmTarget = JavaVersion.VERSION_11.toString()
-//    }
-//
-//    defaultConfig {
-//        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-//        applicationId = "com.example.delivery_app"
-//        // You can update the following values to match your application needs.
-//        // For more information, see: https://flutter.dev/to/review-gradle-config.
-//        minSdk = flutter.minSdkVersion
-//        targetSdk = flutter.targetSdkVersion
-//        versionCode = flutter.versionCode
-//        versionName = flutter.versionName
-//    }
-//
-//    buildTypes {
-//        release {
-//            // TODO: Add your own signing config for the release build.
-//            // Signing with the debug keys for now, so `flutter run --release` works.
-//            signingConfig = signingConfigs.getByName("debug")
-//        }
-//    }
-//}
-//
-//flutter {
-//    source = "../.."
-//}
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -54,7 +10,7 @@ android {
     compileSdk = flutter.compileSdkVersion;
 //    ndkVersion = flutter.ndkVersion
 //    ndkVersion = "27.0.12077973"
-    ndkVersion "25.1.8937393";
+    ndkVersion = "25.1.8937393";
     compileSdk = 35;
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8;
@@ -69,16 +25,6 @@ android {
     sourceSets {
         main.java.srcDirs += 'src/main/kotlin';
     }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_11
-//        targetCompatibility = JavaVersion.VERSION_11
-//        // Enable core library desugaring
-//        isCoreLibraryDesugaringEnabled = true
-//    }
-//
-//    kotlinOptions {
-//        jvmTarget = JavaVersion.VERSION_11.toString()
-//    }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
@@ -92,7 +38,7 @@ android {
         versionName = flutter.versionName
 
         // Enable multidex for large apps
-        multiDexEnabled = true
+//        multiDexEnabled = true
     }
 
     buildTypes {
@@ -113,5 +59,5 @@ dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     // MultiDex support
-    implementation("androidx.multidex:multidex:2.0.1")
+//    implementation("androidx.multidex:multidex:2.0.1")
 }
